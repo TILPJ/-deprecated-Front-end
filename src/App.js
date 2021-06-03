b9f6e2f1042a96c37bd9adca21ac911894e0688f
kyuhyun <kyuhyunhaan@gmail.com>
2021-06-04 01:21:02 +0900

    홈, 마이페이지, TIL 페이지 라우팅 셋팅

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import { Route, Switch } from 'react-router-dom';
 import './index.css';
 
 import Home from './containers/Home';
+import Mypage from './containers/Mypage';
+import TIL from './containers/TIL';
 
 function App() {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route path="/mypage" exact component={Mypage} />
+      <Route path="/til" exact component={TIL} />
     </Switch>
   );
 }
---
+++ src/containers/TIL/Til.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const til = () => {
+  return <div>TIL Page</div>;
+};
+
+export default til;
